92cd0ee34ccde68a6c266db74bdfbc7516528ea8
nibiro <guilhermedecarvalhomachado@gmail.com>
2025-04-16 21:33:23 -0600

    [CLASS-27] - Utilizando Qualifiers

--- estruturaearquitetura/src/main/java/io/github/nibiroo/estruturaearquitetura/montadora/api/TesteFabricaController.java
+++ estruturaearquitetura/src/main/java/io/github/nibiroo/estruturaearquitetura/montadora/api/TesteFabricaController.java
@@ -2,6 +2,7 @@ package io.github.nibiroo.estruturaearquitetura.montadora.api;
 
 import io.github.nibiroo.estruturaearquitetura.montadora.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TesteFabricaController {
 
     @Autowired
+    @Qualifier("motorEletrico")
     private Motor motor;
 
     @PostMapping
--- estruturaearquitetura/src/main/java/io/github/nibiroo/estruturaearquitetura/montadora/configuration/MontadoraConfiguration.java
+++ estruturaearquitetura/src/main/java/io/github/nibiroo/estruturaearquitetura/montadora/configuration/MontadoraConfiguration.java
@@ -8,8 +8,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MontadoraConfiguration {
 
-    @Bean
-    public Motor motor() {
+    @Bean(name = "motorAspirado")
+    public Motor motorAspirado() {
         var motor = new Motor();
 
         motor.setCavalos(300);
@@ -17,6 +17,32 @@ public class MontadoraConfiguration {
         motor.setModelo("XPTO-8");
         motor.setLitragem(6.0);
         motor.setTipoMotor(TipoMotor.ASPIRADO);
+
+        return motor;
+    }
+
+    @Bean(name = "motorEletrico")
+    public Motor motorEletrico() {
+        var motor = new Motor();
+
+        motor.setCavalos(450);
+        motor.setCilindros(3);
+        motor.setModelo("TH-80");
+        motor.setLitragem(2.4);
+        motor.setTipoMotor(TipoMotor.ELETRICO);
+
+        return motor;
+    }
+
+    @Bean(name = "motorTurbo")
+    public Motor motorTurbo() {
+        var motor = new Motor();
+
+        motor.setCavalos(700);
+        motor.setCilindros(16);
+        motor.setModelo("VRAU-7");
+        motor.setLitragem(16.0);
+        motor.setTipoMotor(TipoMotor.TURBO);
 
         return motor;
     }
